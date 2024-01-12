b61ab18d24d65776ff06071c6e7ba2bf15ac0be7
KyiLwinOo <khant.bmruby@gmail.comkhant.bmruby@gmail.comkhant.bmruby@gmail.com>
2024-01-12 20:45:18 +0630

    git testing

--- src/main/java/com/carrental/CarRental/ResponseEntity/UserCustomException.java
+++ src/main/java/com/carrental/CarRental/ResponseEntity/UserCustomException.java
@@ -4,5 +4,7 @@ package com.carrental.CarRental.ResponseEntity;
 public class UserCustomException extends RuntimeException{
    public UserCustomException(String message){
         super(message);
+
+
     }
 }
